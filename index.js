f0a018ff3da3aba0ff73ef6bc263841de6a03931
Kiu <zqdummy@gmail.com>
2021-03-09 20:57:59 -0800

    created image route

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT
 // const apiKey = process.env.PEXELS_API_KEY
 // const axios = require('axios')
 
-const getImage = require('./services/getImage')
+const imageRoute = require('./routes/imageRoute')
 
 const express = require('express')
 const app = express()
@@ -13,17 +13,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/static', express.static('public'))
 
-app.get('/image/:count', function (req, res) {
-	const {count} = req.params
-	getImage(count)
-		.then((response) => {
-			// save respond in firestore, then return a copy
-			res.send(response)
-		})
-		.catch((err) => {
-			console.error('An error occurred while requesting an image: ', err)
-		})
-})
+app.use('/image', imageRoute)
 
 app.use(function (req, res, next) {
 	res.status(404).send('Invalid API access')
---
+++ routes/imageRoute.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const imageRoute = express.Router()
+
+const getImage = require('../services/getImage')
+
+imageRoute.get('/', (req, res) => {
+	getImage()
+		.then((response) => {
+			// save respond in firestore, then return a copy
+			res.send(response[0])
+		})
+		.catch((err) => {
+			console.error('An error occurred while requesting an image: ', err)
+		})
+})
+
+imageRoute.get('/:count', (req, res) => {
+	const {count} = req.params
+	getImage(count)
+		.then((response) => {
+			// save respond in firestore, then return a copy
+			res.send(response)
+		})
+		.catch((err) => {
+			console.error('An error occurred while requesting an image: ', err)
+		})
+})
+
+module.exports = imageRoute
