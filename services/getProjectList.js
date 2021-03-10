3c5738b0cdd32ccd4d319fdd555e087dc2e48a8b
Kiu <zqdummy@gmail.com>
2021-03-10 15:37:04 -0800

    add getProjectList

---
+++ services/getProjectList.js
@@ -0,0 +1,14 @@
+const db = require('../firebase/db')
+
+const getProjectList = async (userID, projectID) => {
+	const projectsRef = db.collection('users').doc(userID).collection('projects')
+
+	const snapshot = await projectsRef.get()
+	if (snapshot.empty) {
+		throw new Error('No matching documents.')
+	}
+	// TODO: specify exactly what properties to return
+	return snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}))
+}
+
+module.exports = getProjectList
