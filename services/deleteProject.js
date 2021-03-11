d2d0616690113d25fcc01ab7bc85d5e789bcfaaf
Kiu <zqdummy@gmail.com>
2021-03-10 21:10:26 -0800

    refactor deleteProject

--- services/deleteProject.js
+++ services/deleteProject.js
@@ -1,5 +1,7 @@
 const db = require('../firebase/db')
 
+const deleteCollection = require('./deleteCollection')
+
 const deleteProject = async (userID, projectID) => {
 	try {
 		await deleteCollection(
@@ -20,37 +22,3 @@ const deleteProject = async (userID, projectID) => {
 }
 
 module.exports = deleteProject
-
-// from firestore docs -> need to delete the doc's subcollection before deleting the doc
-async function deleteCollection(db, collectionPath, batchSize) {
-	const collectionRef = db.collection(collectionPath)
-	const query = collectionRef.orderBy('__name__').limit(batchSize)
-
-	return new Promise((resolve, reject) => {
-		deleteQueryBatch(db, query, resolve).catch(reject)
-	})
-}
-
-async function deleteQueryBatch(db, query, resolve) {
-	const snapshot = await query.get()
-
-	const batchSize = snapshot.size
-	if (batchSize === 0) {
-		// When there are no documents left, we are done
-		resolve()
-		return
-	}
-
-	// Delete documents in a batch
-	const batch = db.batch()
-	snapshot.docs.forEach((doc) => {
-		batch.delete(doc.ref)
-	})
-	await batch.commit()
-
-	// Recurse on the next process tick, to avoid
-	// exploding the stack.
-	process.nextTick(() => {
-		deleteQueryBatch(db, query, resolve)
-	})
-}
