a4463f0a8fd64238fa42a62fece1417a5d29c7ed
Kiu <zqdummy@gmail.com>
2021-03-11 22:00:04 -0800

    minor update

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from 'react'
 
-import axios from "axios";
+import axios from 'axios'
 
 const options = {
-	method: "GET",
+	method: 'GET',
 	headers: {
-		Accept: "application/json",
-		"Content-Type": "application/json",
-		Authorization: "563492ad6f917000010000012b8de3d55d21428ea615e911741ea96e",
+		Accept: 'application/json',
+		'Content-Type': 'application/json',
+		Authorization: '',
 	},
-};
+}
 
 export default function Header() {
 	// const [img, setImg] = useState("");
@@ -17,7 +17,7 @@ export default function Header() {
 	useEffect(() => {
 		fetch(`https://api.pexels.com/v1/photo`, options).then((response) =>
 			console.log(response.data)
-		);
+		)
 		// axios
 		// 	.get("https://api.pexels.com/v1", {
 		// 		headers: {
@@ -31,11 +31,11 @@ export default function Header() {
 		// 	.then(({ data }) => {
 		// 		console.log(data);
 		// 	});
-	}, []);
+	}, [])
 
 	return (
 		<header>
 			<h1>Hello Kiu</h1>
 		</header>
-	);
+	)
 }
