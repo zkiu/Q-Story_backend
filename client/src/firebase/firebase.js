58e98f1181ac39af69077638ac3da6109cfbc859
Kiu <zqdummy@gmail.com>
2021-03-13 12:24:12 -0800

    add firestore config to client

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -2,6 +2,7 @@ import {firebaseConfig} from './firebaseConfig'
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/firestore'
 
 // Initialize Firebase
 export const fb = firebase.initializeApp(firebaseConfig)
