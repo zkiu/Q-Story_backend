48cc7790fc9a5ac27acd3f963331360bf1af1675
Kiu <zqdummy@gmail.com>
2021-03-13 12:24:33 -0800

    clean up comments

--- client/src/services/auth/signIn.js
+++ client/src/services/auth/signIn.js
@@ -1,10 +1,8 @@
-// import firebase from 'firebase/app'
 import {fb} from '../../firebase/firebase'
 
-// Wrapper for Firebase api
+// -- Wrapper for Firebase api - https://firebase.google.com/docs/auth/web/password-auth?authuser=1#sign_in_a_user_with_an_email_address_and_password
 export const signIn = async (email, password) => {
 	try {
-		// let userCredentials = await firebase
 		let userCredentials = await fb
 			.auth()
 			.signInWithEmailAndPassword(email, password)
