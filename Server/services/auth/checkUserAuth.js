ce408b3c4b231360b9f2cd727974ac1116682d10
Kiu <zqdummy@gmail.com>
2021-03-15 23:15:47 -0700

    done server checkUserAuth service

--- Server/services/auth/checkUserAuth.js
+++ Server/services/auth/checkUserAuth.js
@@ -1,18 +1,17 @@
-// TODO: use this to check access rights for user for some endpoint access
 const auth = require('../../firebase/auth')
 
 // Check id of user
 // -- https://firebase.google.com/docs/auth/admin/verify-id-tokens?authuser=1#retrieve_id_tokens_on_clients
 // -- https://firebase.google.com/docs/auth/admin/verify-id-tokens?authuser=1#verify_id_tokens_using_the_firebase_admin_sdk
 
-// TODO: need to convert this to async await?
-function checkUserAuth(reqData) {
-	// idToken comes from the client app
-	admin
-		.auth()
-		.verifyIdToken(idToken)
+function checkUserAuth(req) {
+	const {token} = req.headers
+
+	return auth
+		.verifyIdToken(token)
 		.then((decodedToken) => {
-			return (uid = decodedToken.uid)
+			// returns userID
+			return decodedToken.uid
 		})
 		.catch((error) => {
 			throw error
