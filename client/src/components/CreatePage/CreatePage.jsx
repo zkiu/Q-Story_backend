5e4865b12dcb8385dcaeea13389c19e88e78d2e0
Kiu <zqdummy@gmail.com>
2021-03-15 23:31:37 -0700

    add project options container to CreatePage

--- client/src/components/CreatePage/CreatePage.jsx
+++ client/src/components/CreatePage/CreatePage.jsx
@@ -2,9 +2,14 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 import DnDComp from '../DnDComp/DnDComp'
+import SaveBtn from '../SaveBtn/SaveBtn'
+import {useLoginStatus} from '../../services/auth/useLoginStatus'
 
 export default function CreatePage() {
 	const [cards, setCards] = useState([])
+	const [title, setTitle] = useState('Your Title')
+
+	const user = useLoginStatus()
 
 	// -- loading 5 new cards when 1st mounted
 	useEffect(() => {
@@ -18,11 +23,18 @@ export default function CreatePage() {
 	}, [])
 
 	return (
-		<section>
+		<section className="CreatePage">
 			<span></span>
 			{cards && <DnDComp cards={cards} setCards={setCards} />}
 			{/* <CardEditComp cards={cards} setCards={setCards} /> */}
 			{/* <ProjectOptions cards={cards} setCards={setCards} /> */}
+			<div className="optionsContainer">
+				<p>You are login as {user?.displayName}</p>
+				<SaveBtn cards={cards} title={title} />
+			</div>
 		</section>
 	)
 }
+
+// TODO: check no imageID duplicate when loading a new picture
+// TODO: used userid for the backend access by sending token in header or body
