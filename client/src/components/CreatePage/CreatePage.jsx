529e06df5e91179181fc037884960129b6d2fb6c
Kiu <zqdummy@gmail.com>
2021-03-16 22:18:49 -0700

    update CreatePage useEffect to load project by id

--- client/src/components/CreatePage/CreatePage.jsx
+++ client/src/components/CreatePage/CreatePage.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
+import {fb} from '../../firebase/firebase'
 import DnDComp from '../DnDComp/DnDComp'
 import SaveBtn from '../SaveBtn/SaveBtn'
 import {useLoginStatus} from '../../services/auth/useLoginStatus'
@@ -8,19 +9,43 @@ import {useLoginStatus} from '../../services/auth/useLoginStatus'
 export default function CreatePage() {
 	const [cards, setCards] = useState([])
 	const [title, setTitle] = useState('Your Title')
+	const [projectID, setProjectID] = useState('66oc5xB7qEJxQS3ddf59')
 
 	const user = useLoginStatus()
 
 	// -- loading 5 new cards when 1st mounted
 	useEffect(() => {
-		axios.get('http://localhost:8080/image/5').then((response) => {
-			const newCards = response.data.map((item) => {
-				item.paragraph = 'Your story here.'
-				return item
+		// TODO: may be other conditions not accounted for in this useEffect
+		if (projectID.length === 0) {
+			console.log('here false')
+			axios.get('http://localhost:8080/image/5').then((response) => {
+				const newCards = response.data.map((item) => {
+					item.paragraph = 'Your story here.'
+					return item
+				})
+				setCards(newCards)
 			})
-			setCards(newCards)
-		})
-	}, [])
+		} else {
+			if (fb.auth().currentUser && projectID.length !== 0) {
+				fb.auth()
+					.currentUser.getIdToken()
+					.then((token) => {
+						return axios.get(`http://localhost:8080/project/${projectID}`, {
+							headers: {token},
+						})
+					})
+					.then(({data}) => {
+						setCards(data.cards)
+						setTitle(data.title)
+					})
+
+					.catch((err) => {
+						console.error(err)
+						alert('An error has occured. You are likely not logged in.')
+					})
+			}
+		}
+	}, [projectID, user])
 
 	return (
 		<section className="CreatePage">
@@ -30,11 +55,11 @@ export default function CreatePage() {
 			{/* <ProjectOptions cards={cards} setCards={setCards} /> */}
 			<div className="optionsContainer">
 				<p>You are login as {user?.displayName}</p>
-				<SaveBtn cards={cards} title={title} />
+				<SaveBtn cards={cards} title={title} projectID={projectID} />
 			</div>
 		</section>
 	)
 }
 
-// TODO: check no imageID duplicate when loading a new picture
+// TODO: check no imageID duplicate when loading a new picture from API
 // TODO: used userid for the backend access by sending token in header or body
