8c79ecde096ecc23b4d7fa31368b5fee4ffd87d6
Kiu <zqdummy@gmail.com>
2021-03-16 20:41:58 -0700

    fix server batch.commit() issue with saveCards

--- Server/services/firestore/saveCards.js
+++ Server/services/firestore/saveCards.js
@@ -1,18 +1,19 @@
 const db = require('../../firebase/db')
-const batch = db.batch()
 
 const saveCards = async (userID, projectID, reqData) => {
 	const {cards} = reqData
 
-	cards.forEach((doc) => {
-		var docRef = db
-			.collection(`users/${userID}/projects/${projectID}/cards`)
-			.doc() //automatically generate unique id
-		batch.set(docRef, doc)
-	})
-
+	// ! db.batch() needs to be in local scope. If put in the global scope (like right on line 2) then this happpens -> the first time that function runs, it calls batch.commit() and it commits all changes. So, when that function runs again, we get that error because commit was already called for that instance.
+	const batch = db.batch()
 	try {
-		batch.commit()
+		cards.forEach((doc) => {
+			var docRef = db
+				.collection(`users/${userID}/projects/${projectID}/cards`)
+				.doc() //automatically generate unique id
+			batch.set(docRef, doc)
+		})
+		await batch.commit()
+		return {message: 'cards saved'}
 	} catch (error) {
 		throw error
 	}
