0eb4432621285087acdad9f8be673a9ad2386429
Kiu <zqdummy@gmail.com>
2021-03-16 23:38:05 -0700

    add dummy AboutPage

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 import Header from './Header/Header'
 import CreatePage from './CreatePage/CreatePage'
+import AboutPage from './AboutPage/AboutPage'
 
 class App extends Component {
 	render() {
@@ -10,13 +11,21 @@ class App extends Component {
 			<Router>
 				<Header />
 				<Switch>
-					<Route exact path="/" component={CreatePage} />
+					{/* <Route exact path="/" component={CreatePage} /> */}
 					{/* <Route exact path="/theater/:id" component={TheaterPage} />
 					<Route exact path="/about" component={AboutPage} /> */}
+					<Route exact path="/">
+						<CreatePage />
+					</Route>
+					<Route exact path="/about">
+						<AboutPage />
+					</Route>
 				</Switch>
 			</Router>
 		)
 	}
 }
+
+// TODO: use animated transition between create and theater page: https://reactrouter.com/web/example/animated-transitions
 
 export default App
