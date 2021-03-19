de65a5563ab012600c2297c51224d5661d7f1804
Kiu <zqdummy@gmail.com>
2021-03-19 13:00:50 -0700

    adjust className for CarouselSlides

--- client/src/components/CarouselSlides/CarouselSlides.jsx
+++ client/src/components/CarouselSlides/CarouselSlides.jsx
@@ -1,16 +1,20 @@
 export default function CarouselSlides({cards}) {
 	const slideEl = cards.map((card, index) => {
 		return (
-			<>
-				<div
-					className={index === 0 ? 'carousel-item active' : 'carousel-item'}
-					key={index}
-				>
-					<div className="slideCard">
-						<img src={card.imgLag} className="d-block w-100" alt="" />
-					</div>
+			<div
+				className={index === 0 ? 'carousel-item active' : 'carousel-item'}
+				key={index}
+			>
+				<div className="slideCard">
+					<img
+						src={card.imgLag}
+						className={
+							card.width >= card.height ? 'd-block w-100' : 'd-block h-100'
+						}
+						alt=""
+					/>
 				</div>
-			</>
+			</div>
 		)
 	})
 
