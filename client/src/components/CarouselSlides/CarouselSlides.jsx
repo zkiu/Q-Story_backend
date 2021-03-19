f4ad20daf52001146765d378b7150258c2c42b21
Kiu <zqdummy@gmail.com>
2021-03-19 12:40:42 -0700

    refactor CarouselSlides.jsx

--- client/src/components/CarouselSlides/CarouselSlides.jsx
+++ client/src/components/CarouselSlides/CarouselSlides.jsx
@@ -1,30 +1,17 @@
 export default function CarouselSlides({cards}) {
 	const slideEl = cards.map((card, index) => {
-		if (index === 0) {
-			return (
-				<div className="carousel-item active" key={index}>
+		return (
+			<>
+				<div
+					className={index === 0 ? 'carousel-item active' : 'carousel-item'}
+					key={index}
+				>
 					<div className="slideCard">
 						<img src={card.imgLag} className="d-block w-100" alt="" />
-						{/* <div className="carousel-caption">
-							<p>{card.paragraph}</p>
-						</div> */}
 					</div>
 				</div>
-			)
-		} else {
-			return (
-				<div className="carousel-item" key={index}>
-					<div className="slideCard">
-						<img src={card.imgLag} className="d-block w-100" alt="" />
-						{/* <div className="carousel-caption"> */}
-						{/* <div className="carousel-caption d-none d-md-block"> */}
-						{/* <h5>Second slide label</h5> */}
-						{/* <p>{card.paragraph}</p>
-						</div> */}
-					</div>
-				</div>
-			)
-		}
+			</>
+		)
 	})
 
 	return <div className="carousel-inner">{slideEl}</div>
