09563e7d9f1587e015c19b0455ef878a46c73a03
Kiu <zqdummy@gmail.com>
2021-03-19 13:07:31 -0700

    adjust carousel indicator position

--- client/src/components/CarouselIndicator/CarouselIndicator.jsx
+++ client/src/components/CarouselIndicator/CarouselIndicator.jsx
@@ -1,28 +1,16 @@
 export default function CarouselIndicator({cards}) {
 	const buttonEl = cards.map((card, index) => {
-		if (index === 0) {
-			return (
-				<button
-					type="button"
-					data-bs-target="#carouselExampleCaptions"
-					data-bs-slide-to={index}
-					className="active"
-					aria-current="true"
-					aria-label={'Slide ' + (index + 1)}
-					key={index}
-				></button>
-			)
-		} else {
-			return (
-				<button
-					type="button"
-					data-bs-target="#carouselExampleCaptions"
-					data-bs-slide-to={index}
-					aria-label={'Slide ' + (index + 1)}
-					key={index}
-				></button>
-			)
-		}
+		return (
+			<button
+				type="button"
+				data-bs-target="#carouselExampleCaptions"
+				data-bs-slide-to={index}
+				aria-label={'Slide ' + (index + 1)}
+				key={index}
+				className={index === 0 ? 'active' : ''}
+				aria-current={index === 0 ? 'true' : 'false'}
+			></button>
+		)
 	})
 
 	return <div className="carousel-indicators">{buttonEl}</div>
