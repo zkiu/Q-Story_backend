d6d1007a7b7fb72496d0a124d44eca45650d6888
Kiu <zqdummy@gmail.com>
2021-03-20 16:45:51 -0700

    set over all viewport on app.scss

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,28 +9,30 @@ import TheaterPage from './TheaterPage/TheaterPage'
 class App extends Component {
 	render() {
 		return (
-			<Router>
-				<Switch>
-					{/* <Route exact path="/" component={HomePage} /> */}
-					{/* <Route exact path="/theater/:id" component={TheaterPage} />
+			<main>
+				<Router>
+					<Switch>
+						{/* <Route exact path="/" component={HomePage} /> */}
+						{/* <Route exact path="/theater/:id" component={TheaterPage} />
 					<Route exact path="/about" component={AboutPage} /> */}
-					<Route exact path="/">
-						<Header />
-						<HomePage />
-					</Route>
-					<Route exact path="/project/:projectid">
-						<Header />
-						<HomePage />
-					</Route>
-					<Route exact path="/about">
-						<Header />
-						<AboutPage />
-					</Route>
-					<Route exact path="/theater/:projectid">
-						<TheaterPage />
-					</Route>
-				</Switch>
-			</Router>
+						<Route exact path="/">
+							<Header />
+							<HomePage />
+						</Route>
+						<Route exact path="/project/:projectid">
+							<Header />
+							<HomePage />
+						</Route>
+						<Route exact path="/about">
+							<Header />
+							<AboutPage />
+						</Route>
+						<Route exact path="/theater/:projectid">
+							<TheaterPage />
+						</Route>
+					</Switch>
+				</Router>
+			</main>
 		)
 	}
 }
