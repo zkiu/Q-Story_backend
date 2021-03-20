190bab119946faa031e7eeea0e0a5490e663e519
Kiu <zqdummy@gmail.com>
2021-03-19 18:17:43 -0700

    major refactor useEffect of HomePage

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -18,30 +18,61 @@ import EditCardComp from '../EditCardComp/EditCardComp'
 export default function HomePage() {
 	const [cards, setCards] = useState([])
 	const [title, setTitle] = useState('')
-	const [projectID, setProjectID] = useState('')
 	const [imageIndex, setImageIndex] = useState(null)
 
 	let history = useHistory()
-	let {projectid} = useParams()
+	let {projectid} = useParams() // either undefined or has projectid param
 	const {isLoading, userInfo} = useLoginStatus()
 
+	// -- on homepage (regardless of log status)
 	useEffect(() => {
-		// console.log(projectid)
-		if (projectid && user) {
-			// console.log('1st effect: with param and user')
-			setProjectID(projectid)
-			setImageIndex(null)
+		if (!isLoading && projectid == null) {
+			console.log('1st effect on homepage (regardless of log status)')
+			// console.log('3rd effect: no projectID')
+			axios
+				.get('http://localhost:8080/image/5')
+				.then((response) => {
+					const newCards = response.data.map((item) => {
+						item.paragraph = 'Your story here.'
+						return item
+					})
+					setCards(newCards)
+					setTitle('')
+					setImageIndex(null)
+				})
+				.catch((err) => {
+					alert(
+						'An error occurred while requesting an image. Please check the console'
+					)
+					console.error('An error occurred while requesting an image: ', err)
+				})
 		}
-	}, [projectid, user])
+		// ! don't add user as dependency since it will reload whenever login status change, and update the picture
+	}, [isLoading, projectid])
 
+	// -- NOT logged && on projectid
 	useEffect(() => {
-		if (projectID.length !== 0 && user) {
-			// console.log('2nd effect with projectID and user ')
+		// -- when loading is complete && user is null (when just opening the app or signing OUT) BUT projectid is true
+		if (!isLoading && !userInfo && projectid) {
+			console.log('2nd effect NOT logged && on projectid')
+			// reset everything
+			setCards([])
+			setTitle('')
+			setImageIndex(null)
+			history.push(`/`)
+			alert('please log-in first to access this link')
+		}
+	}, [isLoading, userInfo, projectid, history])
 
+	// -- Logged IN && on projectid
+	useEffect(() => {
+		// -- when loading is complete && user exist && projectid exist
+		if (!isLoading && userInfo && projectid) {
+			console.log('3rd effect Logged IN && on projectid')
 			fb.auth()
-				.currentUser?.getIdToken()
+				.currentUser.getIdToken()
 				.then((token) => {
-					return axios.get(`http://localhost:8080/project/${projectID}`, {
+					return axios.get(`http://localhost:8080/project/${projectid}`, {
 						headers: {token},
 					})
 				})
@@ -52,39 +83,17 @@ export default function HomePage() {
 				})
 				.catch((err) => {
 					console.error(err)
-					alert('An error has occured. You are likely not logged in.')
+					alert(err)
 				})
 		}
-	}, [projectID, user])
-
-	useEffect(() => {
-		if (projectID.length === 0) {
-			// console.log('3rd effect: no project id')
-			axios
-				.get('http://localhost:8080/image/5')
-				.then((response) => {
-					const newCards = response.data.map((item) => {
-						item.paragraph = 'Your story here.'
-						return item
-					})
-					setCards(newCards)
-					setImageIndex(null)
-				})
-				.catch((err) => {
-					alert(
-						'An error occurred while requesting an image. Please check the console'
-					)
-					console.error('An error occurred while requesting an image: ', err)
-				})
-		}
-	}, [projectID])
+	}, [isLoading, userInfo, projectid])
 
 	const handleTheaterMode = (e) => {
 		e.preventDefault()
-		if (projectID.length === 0) {
-			alert('You must save the project 1st.')
+		if (projectid == null) {
+			alert('You must save the project first.')
 		} else {
-			history.push(`/theater/${projectID}`)
+			history.push(`/theater/${projectid}`)
 		}
 	}
 
@@ -99,39 +108,36 @@ export default function HomePage() {
 					setImageIndex={setImageIndex}
 				/>
 			)}
-			{/* <CardEditComp cards={cards} setCards={setCards} /> */}
-			{/* <ProjectOptions cards={cards} setCards={setCards} /> */}
 			<div className="toolContainer">
 				<AddCardBtn cards={cards} setCards={setCards} />
 				<ResetBtn setCards={setCards} setImageIndex={setImageIndex} />
 			</div>
+			{/* ******************************************** */}
 			<div className="guideContainer">
 				<span>Start</span>
 				<span>End</span>
 			</div>
+			{/* ******************************************** */}
 			<div className="optionsContainer">
-				<p>You are login as {user?.displayName}</p>
-				{user && <ListModal setProjectID={setProjectID} user={user} />}
-				<SaveBtn
-					cards={cards}
-					title={title}
-					projectID={projectID}
-					setProjectID={setProjectID}
-				/>
+				<p>You are login as {userInfo?.displayName}</p>
+				{userInfo && <ListModal />}
+				{/* {userInfo && <ListModal setProjectID={setProjectID} userInfo={userInfo} />} */}
+				<SaveBtn cards={cards} title={title} projectid={projectid} />
 				<NewBtn
-					setProjectID={setProjectID}
 					setCards={setCards}
 					setTitle={setTitle}
 					setImageIndex={setImageIndex}
 				/>
 				<AboutBtn />
 			</div>
+			{/* ******************************************** */}
 			<EditCardComp
 				cards={cards}
 				setCards={setCards}
 				imageIndex={imageIndex}
 				setImageIndex={setImageIndex}
 			/>
+			{/* ******************************************** */}
 			<div className="theaterContainer">
 				<div className="btn btn-primary" onClick={handleTheaterMode}>
 					<span>Theater Mode</span>
