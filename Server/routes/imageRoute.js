62100dcd8da4222e7b5531f565684d5bfd878fa6
Kiu <zqdummy@gmail.com>
2021-03-20 15:17:35 -0700

    revert imageRoute to use Promise.All

--- Server/routes/imageRoute.js
+++ Server/routes/imageRoute.js
@@ -40,38 +40,32 @@ imageRoute.get('/:count', (req, res) => {
 		})
 })
 
-// imageRoute.get('/:count', (req, res) => {
-// 	const {count} = req.params
-// 	let arr = Array(+count)
-// 		.fill(null)
-// 		.map(() => {
-// 			return getImage()
-// 		})
-// 	Promise.all(arr)
-// 		.then((results) => {
-// 			return results.map((result) => result[0])
-// 		})
-// 		.then((responses) => {
-// 			res.send(responses)
-// 		})
-// 		.catch((err) => {
-// 			if (err.response.status === 429) {
-// 				res.status(429).json({
-// 					err: err.response.data,
-// 					message:
-// 						'The 3rd party image API limit has been reached. The poor developer (Kiu) will need income to subscribed to a paid tier of the 3rd party API to increase image availability for this app. Contact me if you want to donate.',
-// 				})
-// 			} else {
-// 				// console.error('An error occurred while requesting an image: ', err)
-// 				res.json(err)
-// 			}
-// 		})
-// })
-// // imageRoute.get('/:count', (req, res) => {
-// // 	const {count} = req.params
-// // 	getImage(count).then((responses) => {
-// // 		res.send(responses)
-// // 	})
-// // })
+imageRoute.get('/:count', (req, res) => {
+	const {count} = req.params
+	let arr = Array(+count)
+		.fill(null)
+		.map(() => {
+			return getImage()
+		})
+	Promise.all(arr)
+		.then((results) => {
+			return results.map((result) => result[0])
+		})
+		.then((responses) => {
+			res.send(responses)
+		})
+		.catch((err) => {
+			if (err.response.status === 429) {
+				res.status(429).json({
+					err: err.response.data,
+					message:
+						'The 3rd party image API limit has been reached. The poor developer (Kiu) will need income to subscribed to a paid tier of the 3rd party API to increase image availability for this app. Contact me if you want to donate.',
+				})
+			} else {
+				// console.error('An error occurred while requesting an image: ', err)
+				res.json(err)
+			}
+		})
+})
 
 module.exports = imageRoute
