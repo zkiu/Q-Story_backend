fc7da4106508b3e8517651b40d1eb14915fe6b7a
Kiu <zqdummy@gmail.com>
2021-03-19 18:15:27 -0700

    fix error with CurrentUserComp

--- client/src/components/CurrentUserComp/CurrentUserComp.jsx
+++ client/src/components/CurrentUserComp/CurrentUserComp.jsx
@@ -2,11 +2,11 @@ import {useLoginStatus} from '../../services/auth/useLoginStatus'
 
 export default function CurrentUserComp() {
 	const {isLoading, userInfo} = useLoginStatus()
-
+	// userInfo will be null for a brief moment when login out. this will cause an error. hence ? was place before the displayName property
 	return (
 		<>
 			<span className="welcome-message">
-				Hello {!isLoading && userInfo.displayName}!
+				Hello {!isLoading && userInfo?.displayName}!
 			</span>
 		</>
 	)
