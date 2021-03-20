e95d4a7d6c0a941659af8c28b98cffa61cbed927
Kiu <zqdummy@gmail.com>
2021-03-19 18:17:10 -0700

    redirect before signingout

--- client/src/components/SignOutBtn/SignOutBtn.jsx
+++ client/src/components/SignOutBtn/SignOutBtn.jsx
@@ -1,8 +1,12 @@
+import {useHistory} from 'react-router-dom'
 import {signOut} from '../../services/auth/signOut'
 
 export default function SignOutBtn() {
+	let history = useHistory()
+
 	function handleClick(e) {
 		e.preventDefault()
+		history.push('/')
 		signOut()
 			.then((result) => {
 				// TODO: add toast
