d7cc2d12471f2dd14e255e610290f10ca3ab1693
Kiu <zqdummy@gmail.com>
2021-03-21 15:26:58 -0700

    update EditCardComp style

--- client/src/components/EditCardComp/EditCardComp.jsx
+++ client/src/components/EditCardComp/EditCardComp.jsx
@@ -1,4 +1,3 @@
-// import React, {useState, useEffect} from 'react'
 import DeleteCardBtn from '../DeleteCardBtn/DeleteCardBtn'
 import SwitchImageBtn from '../SwitchImageBtn/SwitchImageBtn'
 
@@ -8,52 +7,7 @@ export default function EditCardComp({
 	imageIndex,
 	setImageIndex,
 }) {
-	// const [card, setCard] = useState(() => {
-	// 	if (imageIndex !== null) {
-	// 		return cards[imageIndex]
-	// 	} else {
-	// 		return {
-	// 			imageID: '',
-	// 			imgSmall: '',
-	// 			imgMed: '',
-	// 			imgLag: '',
-	// 			width: '',
-	// 			height: '',
-	// 			paragraph: '',
-	// 		}
-	// 	}
-	// })
-
-	// useEffect(() => {
-	// 	if (imageIndex !== null) {
-	// 		let info = cards[imageIndex]
-	// 		setCard(info)
-	// 	}
-	// }, [imageIndex, cards])
-
-	// function handleChange(e) {
-	// setCard({
-	// 	...card,
-	// 	[e.target.name]: e.target.value,
-	// })
-	// const formEl = document.querySelector('form[name=cardform]')
-	// // console.log(formEl)
-	// formEl.submit()
-	// // const temp = [...cards]
-	// // temp.splice(imageIndex, 1, card)
-	// // setCards(temp)
-	// var event = new Event('submit', {
-	// 	bubbles: true,
-	// 	cancelable: true,
-	// })
-	// formEl.dispatchEvent(event)
-	// }
 	function handleChange(e) {
-		// setCard({
-		// 	...card,
-		// 	[e.target.name]: e.target.value,
-		// })
-
 		const temp = [...cards]
 
 		temp[imageIndex] = {
@@ -63,80 +17,59 @@ export default function EditCardComp({
 
 		setCards(temp)
 	}
-
-	/*******************************************************************/
-	// function submitEdit() {
-	// 	var ele = document.getElementById('edit')
-	// 	ele.submit()
-
-	// 	var event = new Event('submit', {
-	// 		bubbles: true,
-	// 		cancelable: true,
-	// 	})
-	// 	ele.dispatchEvent(event)
-	// }
-	// submitEdit()
-
-	// function validate() {
-	// 	console.log('form submitted')
-	// }
-	/*******************************************************************/
-
-	// function handleSubmit(e) {
-	// 	e.preventDefault()
-	// 	const temp = [...cards]
-	// 	temp.splice(imageIndex, 1, card)
-	// 	setCards(temp)
-	// 	// TODO: trigger toast to say things are saved
-	// 	// console.log(regData)
-	// }
-
-	// console.log(cards[imageIndex])
-
-	// const info
-	const cardForm = (
+	// const cardForm =
+	return (
 		<>
-			{/* Card info here {imageIndex} */}
 			{cards[imageIndex] != null && (
-				<form
-					className="form-card editcard"
-					// onSubmit={handleSubmit}
-					name="cardform"
-				>
-					<img src={cards[imageIndex].imgMed} alt="" />
-
-					<div className="form-floating">
-						<textarea
-							className="form-control"
-							placeholder="Describe your imagination"
-							id="floatingCardParagraph"
-							name="paragraph"
-							value={cards[imageIndex].paragraph}
-							onChange={handleChange}
-						></textarea>
-						<label htmlFor="floatingCardParagraph">Story Text</label>
+				<form className="editcard">
+					<div
+						className="imageContainer"
+						style={{
+							backgroundImage: `url('${cards[imageIndex].imgMed}')`,
+						}}
+					>
+						{/* <img
+							className="editcardImg"
+							src={cards[imageIndex].imgMed}
+							alt=""
+						/> */}
 					</div>
+					<div className="editCardContainer">
+						<div className="inputGroup">
+							<label className="cardParagraph--label" htmlFor="cardParagraph">
+								Story Text:
+							</label>
+							<textarea
+								className="cardParagraph--input"
+								placeholder="Your story here:"
+								id="cardParagraph"
+								name="paragraph"
+								value={cards[imageIndex].paragraph}
+								onChange={handleChange}
+							></textarea>
+						</div>
 
-					<DeleteCardBtn
-						cards={cards}
-						setCards={setCards}
-						imageIndex={imageIndex}
-						setImageIndex={setImageIndex}
-					/>
-					<SwitchImageBtn
-						cards={cards}
-						setCards={setCards}
-						imageIndex={imageIndex}
-						setImageIndex={setImageIndex}
-					/>
-					{/* <div className="footer">
-						<button type="submit" className="mybtn">
-							Update Text
-						</button>
-					</div> */}
+						<div className="btnContainer">
+							<div className="cardIcon cardIcon--switch">
+								<SwitchImageBtn
+									cards={cards}
+									setCards={setCards}
+									imageIndex={imageIndex}
+									setImageIndex={setImageIndex}
+								/>
+							</div>
+							<div className="cardIcon cardIcon--trash">
+								<DeleteCardBtn
+									cards={cards}
+									setCards={setCards}
+									imageIndex={imageIndex}
+									setImageIndex={setImageIndex}
+								/>
+							</div>
+						</div>
+					</div>
 				</form>
 			)}
 		</>
 	)
-	return cardForm
 }
