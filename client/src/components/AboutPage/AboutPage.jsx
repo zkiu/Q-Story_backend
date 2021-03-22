d4d5d402860eb6e9b1b68ff68caf4998e4612c39
Kiu <zqdummy@gmail.com>
2021-03-22 15:52:54 -0700

    add new contact icons to About Page

--- client/src/components/AboutPage/AboutPage.jsx
+++ client/src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,6 @@
 import {FaGithubAlt, FaLinkedin} from 'react-icons/fa'
+import {GoMailRead} from 'react-icons/go'
+import {HiOutlineBriefcase} from 'react-icons/hi'
 import toolstack from '../../assets/images/toolstack.png'
 
 export default function AboutPage() {
@@ -41,11 +43,17 @@ export default function AboutPage() {
 						</p>
 					</div>
 					<div className="profile-container-link">
+						<a href="https://zkiu.github.io/portfolio/">
+							<HiOutlineBriefcase className="profileBtn" />
+						</a>
 						<a href="https://github.com/zkiu">
 							<FaGithubAlt className="profileBtn" />
 						</a>
 						<a href="https://www.linkedin.com/in/devkiu/">
 							<FaLinkedin className="profileBtn" />
+						</a>
+						<a href="mailto:ukeqlee@gmail.com?subject=qstory">
+							<GoMailRead className="profileBtn" />
 						</a>
 					</div>
 				</div>
