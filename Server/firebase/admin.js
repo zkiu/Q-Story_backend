5b3f99d82d0cddd8c232d9514b8e944e682f0d29
Kiu <zqdummy@gmail.com>
2021-03-22 20:24:55 -0700

    preparing config file & package.json for deploying

--- Server/firebase/admin.js
+++ Server/firebase/admin.js
@@ -1,8 +1,17 @@
 const admin = require('firebase-admin')
 
-const serviceAccount = require('./firebase-adminsdk-key.json')
+// -- for local development
+// const serviceAccount = require('./firebase-adminsdk-key.json')
+// admin.initializeApp({
+// 	credential: admin.credential.cert(serviceAccount),
+// })
 
+//https://stackoverflow.com/questions/41287108/deploying-firebase-app-with-service-account-to-heroku-environment-variables-wit
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
+	credential: admin.credential.cert({
+		projectId: process.env.FIREBASE_PROJECT_ID,
+		private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+		client_email: process.env.FIREBASE_CLIENT_EMAIL,
+	}),
 })
 module.exports = admin
