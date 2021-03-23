65da1b7f36aedfc53664ace969ae1ee04dcd069c
Kiu <zqdummy@gmail.com>
2021-03-22 17:31:49 -0700

    🐛fix with getOneImage.js

--- client/src/services/api/getOneImage.js
+++ client/src/services/api/getOneImage.js
@@ -3,7 +3,16 @@ import axios from 'axios'
 //returns image object
 export const getOneImage = async () => {
 	const {data} = await axios.get('http://localhost:8080/image')
-	return data
+	// return
+	return {
+		paragraph: '',
+		imageID: data.imageID,
+		imgSmall: data.imgSmall,
+		imgMed: data.imgMed,
+		imgLag: data.imgLag,
+		width: data.width,
+		height: data.height,
+	}
 }
 
 // !for testing
