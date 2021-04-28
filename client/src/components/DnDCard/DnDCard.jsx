87d70cc14e21682f411fbb51445021b7e087ae69
Kiu <zqdummy@gmail.com>
2021-04-27 21:47:34 -0700

    add loading spinner to card images

--- client/src/components/DnDCard/DnDCard.jsx
+++ client/src/components/DnDCard/DnDCard.jsx
@@ -1,7 +1,7 @@
 export default function DnDCard({card}) {
 	return (
 		<div className="DnDCard">
-			<img src={card.imgSmall} alt="" />
+			<img src={card.imgSmall} className="imgLoading" alt="" />
 			<div className="textContainer">
 				<p>{card.paragraph}</p>
 			</div>
